0a7952d46a7282ca0cafd650d24567412c3baeae
JonathanViera <jonathan_anunciacao@id.uff.br>
2026-08-14 15:49:42 -0300

    trab1

--- Trab1/Intro.py
+++ Trab1/Intro.py
@@ -7,8 +7,8 @@ pygame.font.init()
 
 
 font = font = pygame.font.Font(None, 50)
-Nome = "Seu Nome"
-rect =  (260, 100, 175, 35)
+Nome = "Jonathan Anunciacao"
+rect =  (390, 130, 365, 50)
 
 random.seed(Nome)
 x, y =  random.randint(0, 500), random.randint(0, 400)
